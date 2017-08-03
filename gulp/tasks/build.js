1221ce3afb29c16033084995dcbf66808511e5d3
Jarod <jarod.hargreaves@gmail.com>
2017-08-03 23:26:03 +0100

    Add image optimisation

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,19 +1,11 @@
 var gulp = require("gulp"),
+	imagemin = require("gulp-imagemin"),
 	del = require("del"),
 	usemin = require("gulp-usemin"),
 	rev = require("gulp-rev"),
 	cssnano = require("gulp-cssnano"),
 	uglify = require("gulp-uglify"),
 	browserSync = require("browser-sync").create();
-
-gulp.task("previewDocs", function(){
-	browserSync.init({
-		notify: false,
-		server: {
-			baseDir: "docs"
-		}
-	});
-});
 
 gulp.task("deleteDocsFolder", function() {
 	return del("./docs");
@@ -23,13 +15,25 @@ gulp.task("copyGeneralFiles", ["deleteDocsFolder"], function() {
 	var pathsToCopy = [
 	"./app/**/*",
 	"!./app/index.html",
-	"!./app/assets/**",
+	"!./app/assets/images/**",
+	"!./app/assets/styles/**",
+	"!./app/assets/scripts/**",
 	"!./app/temp",
 	"!./app/temp/**"
 	];
 	return gulp.src(pathsToCopy)
 	.pipe(gulp.dest("./docs"));
 });
+
+gulp.task("optimiseImages", ["deleteDocsFolder"], function() {
+	return gulp.src(["./app/assets/images/**/*", "!.app/assets/images/icons", "!.app/assets/images/icons/**/*"])
+	.pipe(imagemin({
+		progressive: true,
+		interlaced: true,
+		multipass: true
+	}))
+	.pipe(gulp.dest("./docs/assets/images"))
+})
 
 gulp.task("useminTrigger", ["deleteDocsFolder"], function(){
 	gulp.start("usemin");
@@ -38,10 +42,10 @@ gulp.task("useminTrigger", ["deleteDocsFolder"], function(){
 gulp.task("usemin", ["styles", "scripts"], function() {
 	return gulp.src("./app/index.html")
 	.pipe(usemin({
-		css: [function(){return rev();}, function(){return cssnano();}],
-		js: [function(){return rev();}, function(){return uglify();}]
+		css: [function(){return rev()}, function(){return cssnano()}],
+		js: [function(){return rev()}, function(){return uglify()}]
 	}))
 	.pipe(gulp.dest("./docs"));
 });
 
-gulp.task("build", ["deleteDocsFolder", "useminTrigger", "copyGeneralFiles"]);
+gulp.task("build", ["deleteDocsFolder", "optimiseImages", "useminTrigger", "copyGeneralFiles"]);
