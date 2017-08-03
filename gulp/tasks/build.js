83f5c894258942088d5719f57ad76510a3377b4d
Jarod <jarod.hargreaves@gmail.com>
2017-08-03 23:07:39 +0100

    Initial commit

---
+++ gulp/tasks/build.js
@@ -0,0 +1,47 @@
+var gulp = require("gulp"),
+	del = require("del"),
+	usemin = require("gulp-usemin"),
+	rev = require("gulp-rev"),
+	cssnano = require("gulp-cssnano"),
+	uglify = require("gulp-uglify"),
+	browserSync = require("browser-sync").create();
+
+gulp.task("previewDocs", function(){
+	browserSync.init({
+		notify: false,
+		server: {
+			baseDir: "docs"
+		}
+	});
+});
+
+gulp.task("deleteDocsFolder", function() {
+	return del("./docs");
+});
+
+gulp.task("copyGeneralFiles", ["deleteDocsFolder"], function() {
+	var pathsToCopy = [
+	"./app/**/*",
+	"!./app/index.html",
+	"!./app/assets/**",
+	"!./app/temp",
+	"!./app/temp/**"
+	];
+	return gulp.src(pathsToCopy)
+	.pipe(gulp.dest("./docs"));
+});
+
+gulp.task("useminTrigger", ["deleteDocsFolder"], function(){
+	gulp.start("usemin");
+});
+
+gulp.task("usemin", ["styles", "scripts"], function() {
+	return gulp.src("./app/index.html")
+	.pipe(usemin({
+		css: [function(){return rev();}, function(){return cssnano();}],
+		js: [function(){return rev();}, function(){return uglify();}]
+	}))
+	.pipe(gulp.dest("./docs"));
+});
+
+gulp.task("build", ["deleteDocsFolder", "useminTrigger", "copyGeneralFiles"]);
